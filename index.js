85b6f32e20b86eb182f3c1e65563e5c51e615c0d
benthomasknight <benthomasknight@gmail.com>
2017-06-02 14:34:06 +1000

    Update npmignore for publish

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var dist = require("./dist");
+exports.Comparators = dist.Comparators;
+exports.EncodedQueryBuilder = dist.EncodedQueryBuilder;
+//# sourceMappingURL=index.js.map
